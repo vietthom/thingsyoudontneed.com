ef946520b7ea6cb95cf5063ee7366e7b73a1b64f
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-04-09 11:24:03 -0600

    updating user model with additional properties

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,26 @@ const { model, Schema } = require('mongoose');
 
 //Creates user schema in mongodb
 const userSchema = new Schema({
-    firstName: {type: String},
-    lastName: {type: String},
-    email: {type: String, unique: true},
-    password: {type: String, unique: true},
-    token: {type: String}
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String, 
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String, 
+        unique: true,
+        require: true,
+    },
+    password: {
+        type: String, 
+        unique: true,
+        required: true},
+        token: {type: String}
 });
 
 module.exports = model('User', userSchema);
