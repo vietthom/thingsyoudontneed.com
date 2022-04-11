7bcebbd02ce012f37cc0900458cceacaf0b37209
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-04-10 19:23:33 -0600

    added images to the seeds folder

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -8,7 +8,7 @@ mongo.once('open', async () => {
       {
         productName: "S'more Maker",
         description: "This is a great thing when you want a s`more but don`t want to start a fire!",
-        image:'./images/smoresmaker.jpg',
+        image:'smoresmaker.jpg' ,
         category: categories[0]._id,
         price: 12,
         quantity: 10
@@ -16,7 +16,7 @@ mongo.once('open', async () => {
       {
         productName: "Mini Desk Vacuum",
         description: "No excuses to have your workspace dirty.",
-        image:'',
+        image:'minivacuum.jpg',
         category: categories[0]._id,
         price: 14,
         quantity: 7
@@ -24,7 +24,7 @@ mongo.once('open', async () => {
       {
         productName: "Egg Counter",
         description: "Have your smartphone notify you when you are running low on eggs or when your eggs are going bad!",
-        image:'',
+        image:'eggcounter.jpg',
         category: categories[1]._id,
         price: 11,
         quantity: 10
@@ -34,7 +34,7 @@ mongo.once('open', async () => {
       {
         name: 'Mini Donut Factory',
         description: 'Why struggle making big batches of donuts when you can just make up to 30 mini donuts?!',
-        image:'',
+        image:'donutfactory.jpeg',
         category: categories[1]._id,
         price: 113,
         quantity: 9
@@ -42,7 +42,7 @@ mongo.once('open', async () => {
       {
         name: 'Monogrammed Barbecue Branding Iron',
         description: 'Let your friends and loved ones know who made that delicious piece of steak! Great buy for a grilling fan.',
-        image: '',
+        image: 'ironbrander.jpg',
         category: categories[1]._id,
         price: 17,
         quantity: 8
@@ -50,7 +50,7 @@ mongo.once('open', async () => {
       {
         name: 'Musical Toilet Roll Device',
         description: 'Can`t get enough of Christmas decorations? Have a little fun and sing a long some Christmas tunes!',
-        image: '',
+        image: 'toiletpaperdispenser.jpg',
         category: categories[2]._id,
         price: 8,
         quantity: 5
@@ -58,7 +58,7 @@ mongo.once('open', async () => {
       {
         name: 'Wi-Fi Scent Dispenser',
         description: 'Your phone can do so much now. So why not give it the ability to make any room in your house smell instantly like watermelon Jolly Rancher or sizzling bacon?',
-        image: '',
+        image: 'wifiscentdispenser.jpg',
         category: categories[2]._id,
         price: 108,
         quantity: 15,
@@ -66,7 +66,7 @@ mongo.once('open', async () => {
       {
         name: 'Musical Cake Server',
         description: 'Perfect for eating a birthday cake alone and wishing somebody,even a piece of cutlery, would sing `Happy Birthday` to you.',
-        image: '',
+        image: 'musicalcakeserver.jpg',
         category: categories[3]._id,
         price: 20,
         quantity: 11
@@ -74,7 +74,7 @@ mongo.once('open', async () => {
       {
         name: 'Soft Pretzel Makes With Cheese Dip Warmer',
         description: 'Makes 5 soft pretzels at home with you SuperPretzel brand Soft Pretzel Maker.',
-        image: '',
+        image: 'pretzelmaker.jpg',
         category: categories[4]._id,
         price: 55,
         quantity: 7
@@ -82,7 +82,7 @@ mongo.once('open', async () => {
       {
         name: 'Smartphone Controlled Kitty Water Fountain',
         description: 'In order to prevent diseases and dehydration caused by water intake, we consider that managing your pets` water intake can improve their  health, prevent illness and insure proper hydration',
-        image: '',
+        image: 'waterfountain.jpg',
         category: categories[4]._id,
         price: 27,
         quantity: 9
